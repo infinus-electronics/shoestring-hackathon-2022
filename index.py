a096ca156a4852d9e4256c61830c9162f890187c
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-02-12 17:18:47 +0000

    pruned files

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import dash
 
 from app import app
 from app import server
-from layouts import sales, page2, page3
+from layouts import shelves, page2, page3
 import callbacks
 
 app.layout = html.Div([
@@ -15,14 +15,14 @@ app.layout = html.Div([
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/sales-overview':
-         return sales
+    if pathname == '/apps/shelves-overview':
+         return shelves
     elif pathname == '/apps/page2':
          return page2
     elif pathname == '/apps/page3':
          return page3
     else:
-        return sales # This is the "home page"
+        return shelves # This is the "home page"
 
 if __name__ == '__main__':
     app.run_server(debug=True)
