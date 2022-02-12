6b3f07975ac29633f9aab4fa396fa75ed0083617
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-02-12 17:25:15 +0000

    mqtt subscription for weight

--- mqtt_test.py
+++ mqtt_test.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as paho
 broker="13.40.33.147"
 port=1883
+
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
     pass
@@ -9,4 +10,4 @@ client1= paho.Client("control1")
 client1.username_pw_set(username="myUserAdmin", password="camjfl")                          #create client object
 client1.on_publish = on_publish                          #assign function to callback
 client1.connect(broker,port)                                 #establish connection
-ret= client1.publish("house/bulb1","on")                   #publish
+ret = client1.publish("led","on")                   #publish
