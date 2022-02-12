2835ed3571018a349c98f57f4a6fbb84e82ddf08
anirudhbhalekar <anirudhbhalekar10@gmail.com>
2022-02-12 13:42:37 +0000

    test 2 - mongodb

--- mongoDB_test.py
+++ mongoDB_test.py
@@ -2,10 +2,13 @@ from re import L
 from .Collection_type import object_type
 
 def create_test_collection(db_name, collection_name):
-    dbname = object_type.get_db("test")
-    collecton_name = dbname["test_collection"]
+    dbname = object_type.get_db("{}".format(db_name))
+    collecton_name = dbname["{}".format(collection_name)]
 
+def insert_docs():
+    s = 1
+    
 
 if __name__ == "__main__":
-    create_test_collection()
+    create_test_collection("test","test_c")
 
