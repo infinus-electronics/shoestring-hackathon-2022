c91caa4d225fef773d65e11d5b5b1a54a3decdad
infinus-electronics <70929247+infinus-electronics@users.noreply.github.com>
2022-02-13 11:08:37 +0000

    wsgi py

--- wsgi.py
+++ wsgi.py
@@ -4,25 +4,44 @@ import dash
 
 from app import app
 from app import server
-from layouts import sales, page2, page3
+from layouts import shelves, inv, ana, heat
 import callbacks
 
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+
+def serve_layout():
+
+     print("layout")
+     return html.Div([
+          dcc.Location(id='url', refresh=True),
+          html.Div(id='page-content'),
+          dcc.Interval(
+               id = "interval-component",
+               interval= 10*1000,
+               n_intervals = 0
+          )
+          # dcc.Button(id='invisible_button', style={'display':'none'})
+     ])
+
+app.layout = serve_layout
 
 @app.callback(dash.dependencies.Output('page-content', 'children'),
               [dash.dependencies.Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/apps/sales-overview':
-         return sales
-    elif pathname == '/apps/page2':
-         return page2
-    elif pathname == '/apps/page3':
-         return page3
-    else:
-        return sales # This is the "home page"
-        
-if __name__ == "__main__":
+     print("callback")
+     if pathname == '/apps/shelves-overview':
+          return shelves
+     elif pathname == '/apps/inv':
+          return inv
+     elif pathname == '/apps/ana':
+          return ana
+     elif pathname == '/apps/heat':
+          return heat
+     else:
+          return shelves # This is the "home page"
+
+# @app.routes("/apps/shelves-overview")
+# def shelves():
+#      return shelves
+
+if __name__ == '__main__':
     app.run_server()
