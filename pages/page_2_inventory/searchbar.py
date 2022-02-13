cbad7df22b36b04edb1b554e70b616ea213f08ef
naseweisssss <ryanlin.x.w@hotmail.co.uk>
2022-02-13 12:01:54 +0000

    commiy

--- pages/page_2_inventory/searchbar.py
+++ pages/page_2_inventory/searchbar.py
@@ -9,10 +9,21 @@ from plotly import tools
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
 DATA_PATH = pathlib.Path(__file__).parent.joinpath("data").resolve()
 STARTING_DRUG = "Durian Maggi"
-df = pd.read_csv(DATA_PATH.joinpath("small_molecule_drugbank.csv")).drop(
-    ["Unnamed: 0"], axis=1
-)
+# df = pd.read_csv(DATA_PATH.joinpath("small_molecule_drugbank.csv")).drop(
+#     ["Unnamed: 0"], axis=1
+# )
+
+from pymongo import MongoClient
+from pprint import pprint
+
+client = MongoClient("mongodb://myUserAdmin:camjfl@13.40.33.147:27017")
+db = client.admin
+name = []
+
 def get_searchbar():
+    name = db.list_collection_names()
+    excludes = ['system.version', 'system.users', 'Discounts']
+    name = [c for c in name if c not in excludes]
     searchbar = html.Div(
     [
         
@@ -25,7 +36,7 @@ def get_searchbar():
                             id="chem_dropdown",
                             multi=True,
                             value=[STARTING_DRUG],
-                            options=[{"label": i, "value": i} for i in df["NAME"]],
+                            options=[{"label": i, "value": i} for i in name],
                         )
                     ],
                     className="app__dropdown",
